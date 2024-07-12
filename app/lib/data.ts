9c8d9b6c73e2cd505c021859c1aff8e8d0fc8b35
huy.phamnhat <huy.phamnhat@asnet.com.vn>
2024-07-12 15:25:43 +0700

    feat: adding search and pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,6 +13,12 @@ import {
 import { formatCurrency } from "./utils";
 import axios from "axios";
 import { RESOURCE } from "../constants/resources";
+import {
+  customers,
+  invoices,
+  invoices_table,
+  revenue,
+} from "./placeholder-data";
 
 const http1 = axios.create({
   baseURL: `${process.env.MOCK_API_V1}`,
@@ -22,12 +28,25 @@ const http2 = axios.create({
   baseURL: `${process.env.MOCK_API_V2}`,
 });
 
+function filterByValue<T extends Record<string, any>>(
+  array: Array<T>,
+  value: string
+) {
+  return array.filter((item) =>
+    Object.keys(item).some(
+      (k) =>
+        item[k] != null &&
+        item[k].toString().toLowerCase().includes(value.toLowerCase())
+    )
+  );
+}
+
 export async function fetchRevenue() {
   try {
-    const revenue = (await http1.get<Array<Revenue>>(`/${RESOURCE.REVENUE}`))
-      .data;
-
-    return revenue;
+    const placeHolderRevenue = new Promise<Array<Revenue>>((resolve) => {
+      resolve(revenue);
+    });
+    return placeHolderRevenue;
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch revenue data.");
@@ -37,15 +56,30 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     const latestInvoices = (
-      await http1.get<Array<LatestInvoice>>(
+      await http2.get<Array<Invoice>>(
         `/${RESOURCE.INVOICES}?p=1&l=5&sortBy=date&order=desc`
       )
     ).data;
 
-    const formatCurrencyInvoices = latestInvoices.map((invoice) => ({
+    const latestFormattedInvoices = latestInvoices.map<LatestInvoice>(
+      (invoice) => {
+        const customer = customers.find((c) => c.id === invoice.customer_id);
+
+        return {
+          id: invoice.id,
+          name: customer?.name ?? "",
+          amount: invoice.amount,
+          email: customer?.email ?? "",
+          image_url: customer?.image_url ?? "",
+        };
+      }
+    );
+
+    const formatCurrencyInvoices = latestFormattedInvoices.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(Number(invoice.amount)),
     }));
+
     return formatCurrencyInvoices;
   } catch (error) {
     console.error("Database Error:", error);
@@ -55,18 +89,23 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
-    const invoiceCountPromise = http1.get<Array<LatestInvoice>>(
-      `${RESOURCE.INVOICES}`
-    );
-    const customerCountPromise = http2.get<Array<Customer>>(
-      `${RESOURCE.CUSTOMER}`
-    );
-    const invoiceStatusPromise = http2.get<Array<Invoice>>(
-      `${RESOURCE.INVOICE_STATUS}`
-    );
+    const invoiceCountPromise = new Promise<number>((resolve) => {
+      resolve(invoices.length);
+    });
+
+    const customerCountPromise = new Promise<number>((resolve) => {
+      resolve(customers.length);
+    });
+
+    const invoiceStatusPromise = new Promise<Array<Invoice>>((resolve) => {
+      resolve(
+        invoices.map((invoice) => ({
+          ...invoice,
+          amount: invoice.amount.toString(),
+          status: invoice.status as "paid" | "pending",
+        }))
+      );
+    });
 
     const data = await Promise.all([
       invoiceCountPromise,
@@ -74,15 +113,15 @@ export async function fetchCardData() {
       invoiceStatusPromise,
     ]);
 
-    const numberOfInvoices = Number(data[0].data.length ?? "0");
-    const numberOfCustomers = Number(data[1].data.length ?? "0");
+    const numberOfInvoices = data[0];
+    const numberOfCustomers = data[1];
     const totalPaidInvoices = formatCurrency(
-      data[2].data
+      data[2]
         .filter((invoice) => invoice.status === "paid")
         .reduce((current, next) => current + Number(next.amount), 0) ?? "0"
     );
     const totalPendingInvoices = formatCurrency(
-      data[2].data
+      data[2]
         .filter((invoice) => invoice.status === "pending")
         .reduce((current, next) => current + Number(next.amount), 0) ?? "0"
     );
@@ -104,52 +143,53 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number
 ) {
-  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
-
   try {
-    const invoices = await sql<InvoicesTable>`
-      SELECT
-        invoices.id,
-        invoices.amount,
-        invoices.date,
-        invoices.status,
-        customers.name,
-        customers.email,
-        customers.image_url
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      WHERE
-        customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`} OR
-        invoices.amount::text ILIKE ${`%${query}%`} OR
-        invoices.date::text ILIKE ${`%${query}%`} OR
-        invoices.status ILIKE ${`%${query}%`}
-      ORDER BY invoices.date DESC
-      LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-    `;
+    // const latestInvoicesTable = invoices_table.sort(
+    //   (current, next) => -current.date.localeCompare(next.date)
+    // );
 
-    return invoices.rows;
+    const invoicesTableByQuery = filterByValue(invoices_table, query);
+
+    const latestInvoicesTable = invoicesTableByQuery.sort(
+      (current, next) => -current.date.localeCompare(next.date)
+    );
+
+    const paginatedInvoicesTable = latestInvoicesTable.splice(
+      (currentPage - 1) * ITEMS_PER_PAGE,
+      currentPage * ITEMS_PER_PAGE
+    );
+
+    // const invoicesTable = new Promise<Array<InvoicesTable>>((resolve) => {
+    //   resolve(
+    //     latestInvoices.map<InvoicesTable>((invoice) => {
+    //       const customer = customers.find((c) => c.id === invoice.customer_id);
+    //       return {
+    //         id: invoice.id,
+    //         customer_id: invoice.customer_id,
+    //         name: customer?.name ?? "",
+    //         email: customer?.email ?? "",
+    //         image_url: customer?.image_url ?? "",
+    //         date: invoice.date,
+    //         amount: Number(invoice.amount),
+    //         status: invoice.status as "pending" | "paid",
+    //       };
+    //     })
+    //   );
+    // });
+
+    return paginatedInvoicesTable;
   } catch (error) {
-    console.error("Database Error:", error);
-    throw new Error("Failed to fetch invoices.");
+    return [];
+    // console.error("Database Error:", error);
+    // throw new Error("Failed to fetch invoices.");
   }
 }
 
 export async function fetchInvoicesPages(query: string) {
   try {
-    const count = await sql`SELECT COUNT(*)
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE
-      customers.name ILIKE ${`%${query}%`} OR
-      customers.email ILIKE ${`%${query}%`} OR
-      invoices.amount::text ILIKE ${`%${query}%`} OR
-      invoices.date::text ILIKE ${`%${query}%`} OR
-      invoices.status ILIKE ${`%${query}%`}
-  `;
+    const invoicesTableByQuery = filterByValue(invoices_table, query);
 
-    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
-    return totalPages;
+    return Math.ceil(invoicesTableByQuery.length / ITEMS_PER_PAGE);
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch total number of invoices.");
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,21 +1,26 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { usePathname, useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
 
-    term ? params.set("query", term) : params.delete("query");
+    params.set("page", "1");
 
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
